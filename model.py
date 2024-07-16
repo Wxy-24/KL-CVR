c0618538183ecd0b0220a31bc576461c37f956ad
Xiaoyang Wei <55431079+Wxy-24@users.noreply.github.com>
2024-07-16 22:27:06 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,44 @@
+import os
+import torch
+
+# from simclr import SimCLR
+from modules import LARS
+
+
+def load_optimizer(args, model, lr=3e-4):
+
+    scheduler = None
+    if args.optimizer == "Adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)  # TODO: LARS
+    elif args.optimizer == "LARS":
+        # optimized using LARS with linear learning rate scaling
+        # (i.e. LearningRate = 0.3 × BatchSize/256) and weight decay of 10−6.
+        learning_rate = 0.3 * args.batch_size / 256
+        optimizer = LARS(
+            model.parameters(),
+            lr=learning_rate,
+            weight_decay=args.weight_decay,
+            exclude_from_weight_decay=["batch_normalization", "bias"],
+        )
+
+        # "decay the learning rate with the cosine decay schedule without restarts"
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, args.epochs, eta_min=0, last_epoch=-1
+        )
+    else:
+        raise NotImplementedError
+
+    return optimizer, scheduler
+
+
+def save_model(args, model, optimizer, best=False):
+    if best:
+        out = os.path.join(args.model_path, "best_checkpoint_{}.pth".format(args.current_epoch))
+    else:
+        out = os.path.join(args.model_path, "checkpoint_{}.pth".format(args.current_epoch))
+    # To save a DataParallel model generically, save the model.module.state_dict().
+    # This way, you have the flexibility to load the model any way you want to any device you want.
+    if isinstance(model, torch.nn.DataParallel):
+        torch.save(model.module.state_dict(), out)
+    else:
+        torch.save(model.state_dict(), out)
